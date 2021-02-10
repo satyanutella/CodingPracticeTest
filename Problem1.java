2ba216edb4c2836c48d07bb294c37207e58cb013
Satyapramod <55189106+satyanutella@users.noreply.github.com>
2021-02-10 15:00:07 +0530

    coding test upload

--- Problem1.java
+++ Problem1.java
@@ -2,29 +2,29 @@ package CodingPracticeTest;
 
 import java.util.Scanner;
 
-public class Problem1 {
+public class Problem1 { 
 
 	public static void main(String[] args) {
 		
 		int i;
-		Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in); //inintialising scanner 
 		System.out.println("Enter the range of age : ");
 		int a = scanner.nextInt();
 		
-		int age[] = new int[a];
+		int age[] = new int[a]; // inintialising array
 		
 		System.out.println("Enter your age : ");
 		
-		int count=0,count1=0,count2=0;
+		int count=0,count1=0,count2=0;  //creating count variable
 		
-		for(i=0;i<a;i++)
+		for(i=0;i<a;i++)  //loop 
 		{
 			age[i]=scanner.nextInt();
 		}
 		
 		for(i=0;i<a;i++)
 		{
-			if(age[i]<18)
+			if(age[i]<18) 
 			{
 				count++;
 			}
@@ -40,9 +40,9 @@ public class Problem1 {
 			}
 		}
 		
-		System.out.println("Children : "+count);
-		System.out.println("Adult : "+count1);
-		System.out.println("Senior Citizen : "+count2);
+		System.out.println("Children : "+count); //this will count children < 18 and print
+		System.out.println("Adult : "+count1);  //this will count adult  and print
+		System.out.println("Senior Citizen : "+count2);  //this will count senoir citizen and print
 		
 		scanner.close();
 
