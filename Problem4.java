4416183351ad38c9bce1d1512aedc19f2fbfc0ed
Satyapramod <55189106+satyanutella@users.noreply.github.com>
2021-02-10 15:06:09 +0530

    coding test upload

--- Problem4.java
+++ Problem4.java
@@ -1,27 +1,27 @@
 package CodingPracticeTest;
 
-class Animal 
+class Animal //create class Animal
 {
-	void shout() {
+	void shout() {  //declare method shout
 		System.out.println("Different animal sounds");
 	}
 }
 
-class Dog extends Animal
+class Dog extends Animal   //class Dog extends parent class
 {
 	void shout() {
 		System.out.println(" Dog shouts : Bhaw Bhaw");
 	}
 }
 
-class Horse extends Animal
+class Horse extends Animal  //class Horse extends parent class
 {
 	void shout() {
 		System.out.println(" Horse shouts : kneeee");
 	}
 }
 
-class Cat extends Animal
+class Cat extends Animal  //class Cat extends parent class
 {
 	void shout() {
 		System.out.println("Cat shouts : Meow Meow");
@@ -31,12 +31,12 @@ public class Problem4 {
 
 	public static void main(String[] args) {
 		
-		Animal a = new Dog();
-		Animal a1 = new Horse();
-		Animal a2 = new Cat();
-		a.shout();
-		a1.shout();
-		a2.shout();
+		Animal a = new Dog(); //initialising obj dog
+		Animal a1 = new Horse();  //initialising obj horse
+		Animal a2 = new Cat();  //initialising obj cat
+		a.shout(); //calling method shout() of dog
+		a1.shout(); //calling method shout() of horse
+		a2.shout(); //calling method shout() of cat
 
 	}
 
